001a73aaf78f14c0bb6f44d5a6cb555329c2b412
moonyu <dbwls3681@naver.com>
2024-09-09 13:50:16 +0900

    add undo_like

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -4,3 +4,8 @@ from tabom.models import Like
 def do_like(user_id: int, like_id: int) -> Like:
     like = Like.objects.create(user_id=user_id, article_id=like_id)
     return like
+
+
+def undo_like(user_id: int, article_id: int) -> None:
+    like = Like.objects.filter(user_id=user_id, article_id=article_id).get()
+    like.delete()
